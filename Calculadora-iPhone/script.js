b6336d92f1aa8398bddba72fcf8c6101bcfa371a
Thiago Fraga <thimefra@gmail.com>
2022-08-25 10:10:46 -0300

    inicio do JavaScript

---
+++ Calculadora-iPhone/script.js
@@ -0,0 +1,33 @@
+
+
+const display = document.getElementById('display');
+
+const botaoLimpar = document.getElementById('botaoLimpar');
+const botaoOperador = document.getElementById('botaoOperador');
+const botaoPorcentual = document.getElementById('botaoPorcentual');
+const operadorDividir = document.getElementById('operadorDividir');
+
+const botaoNumero7 = document.getElementById('botaoNumero7');
+const botaoNumero8 = document.getElementById('botaoNumero');
+const botaoNumero9 = document.getElementById('botaoNumero');
+const operadorMultiplicar = document.getElementById('operadorMultiplicar');
+
+const botaoNumero4 = document.getElementById('botaoNumero');
+const botaoNumero5 = document.getElementById('botaoNumero');
+const botaoNumero6 = document.getElementById('botaoNumero');
+const operadorSubtrair = document.getElementById('operadorSubtrair');
+
+const botaoNumero1 = document.getElementById('botaoNumero');
+const botaoNumero2 = document.getElementById('botaoNumero');
+const botaoNumero3 = document.getElementById('botaoNumero');
+const operadorSoma = document.getElementById('operadorSoma');
+
+const botaoNumero0 = document.getElementById('botaoNumero');
+const botaoVirgula = document.getElementById('botaoVirgula');
+const operadorIgualdade = document.getElementById('operadorIgualdade');
+
+function valor7() {
+
+    alert('botaoNumero7')
+
+}

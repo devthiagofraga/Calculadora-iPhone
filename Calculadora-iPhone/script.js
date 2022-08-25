9328a98659092e7386e2883a622b7270e2feb253
Thiago Fraga <thimefra@gmail.com>
2022-08-25 17:28:18 -0300

    atualizacao css hover e JS

--- Calculadora-iPhone/script.js
+++ Calculadora-iPhone/script.js
@@ -8,26 +8,76 @@ const botaoPorcentual = document.getElementById('botaoPorcentual');
 const operadorDividir = document.getElementById('operadorDividir');
 
 const botaoNumero7 = document.getElementById('botaoNumero7');
-const botaoNumero8 = document.getElementById('botaoNumero');
-const botaoNumero9 = document.getElementById('botaoNumero');
-const operadorMultiplicar = document.getElementById('operadorMultiplicar');
-
-const botaoNumero4 = document.getElementById('botaoNumero');
-const botaoNumero5 = document.getElementById('botaoNumero');
-const botaoNumero6 = document.getElementById('botaoNumero');
-const operadorSubtrair = document.getElementById('operadorSubtrair');
-
-const botaoNumero1 = document.getElementById('botaoNumero');
-const botaoNumero2 = document.getElementById('botaoNumero');
-const botaoNumero3 = document.getElementById('botaoNumero');
-const operadorSoma = document.getElementById('operadorSoma');
-
-const botaoNumero0 = document.getElementById('botaoNumero');
-const botaoVirgula = document.getElementById('botaoVirgula');
-const operadorIgualdade = document.getElementById('operadorIgualdade');
 
 function valor7() {
-
-    alert('botaoNumero7')
-
+    const display = document.getElementById('display')
+    display.setAttribute("value", "7")
 }
+const botaoNumero8 = document.getElementById('botaoNumero8');
+
+function valor8() {
+    const display = document.getElementById('display')
+    display.setAttribute("value", "8")
+}
+const botaoNumero9 = document.getElementById('botaoNumero9');
+
+function valor9() {
+    const display = document.getElementById('display')
+    display.setAttribute("value", "9")
+}
+const operadorMultiplicar = document.getElementById('operadorMultiplicar');
+
+const botaoNumero4 = document.getElementById('botaoNumero4');
+
+function valor4() {
+    const display = document.getElementById('display')
+    display.setAttribute("value", "4")
+}
+const botaoNumero5 = document.getElementById('botaoNumero5');
+
+function valor5() {
+    const display = document.getElementById('display')
+    display.setAttribute("value", "5")
+}
+const botaoNumero6 = document.getElementById('botaoNumero6');
+
+function valor6() {
+    const display = document.getElementById('display')
+    display.setAttribute("value", "6")
+}
+const operadorSubtrair = document.getElementById('operadorSubtrair');
+
+const botaoNumero1 = document.getElementById('botaoNumero1');
+
+function valor1() {
+    const display = document.getElementById('display')
+    display.setAttribute("value", "1")
+}
+const botaoNumero2 = document.getElementById('botaoNumero2');
+
+function valor2() {
+    const display = document.getElementById('display')
+    display.setAttribute("value", "2")
+}
+const botaoNumero3 = document.getElementById('botaoNumero3');
+
+function valor3() {
+    const display = document.getElementById('display')
+    display.setAttribute("value", "3")
+}
+const operadorSoma = document.getElementById('operadorSoma');
+
+const botaoNumero0 = document.getElementById('botaoNumero0');
+
+function valor0() {
+    const display = document.getElementById('display')
+    display.setAttribute("value", "0")
+}
+const botaoVirgula = document.getElementById('botaoVirgula');
+
+function virgula() {
+    const display = document.getElementById('display')
+    display.setAttribute("value", ",")
+}
+const operadorIgualdade = document.getElementById('operadorIgualdade');
+

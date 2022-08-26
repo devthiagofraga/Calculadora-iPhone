95ca79150558d54ee47933e33fc9e6e3bc0a7089
Thiago Fraga <thimefra@gmail.com>
2022-08-26 16:27:24 -0300

    finalizando a calculadora

--- Calculadora-iPhone/script.js
+++ Calculadora-iPhone/script.js
@@ -1,83 +1,24 @@
-
-
-const display = document.getElementById('display');
-
-const botaoLimpar = document.getElementById('botaoLimpar');
-const botaoOperador = document.getElementById('botaoOperador');
-const botaoPorcentual = document.getElementById('botaoPorcentual');
-const operadorDividir = document.getElementById('operadorDividir');
-
-const botaoNumero7 = document.getElementById('botaoNumero7');
-
-function valor7() {
-    const display = document.getElementById('display')
-    display.setAttribute("value", "7")
+function insert(numero) {
+    var numero2 = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = numero2 + numero;
 }
-const botaoNumero8 = document.getElementById('botaoNumero8');
 
-function valor8() {
-    const display = document.getElementById('display')
-    display.setAttribute("value", "8")
+function botaoLimpar() {
+    document.getElementById('resultado').innerHTML = "";
 }
-const botaoNumero9 = document.getElementById('botaoNumero9');
-
-function valor9() {
-    const display = document.getElementById('display')
-    display.setAttribute("value", "9")
+function botaoVirgula() {
+    document.getElementById('resultado').innerHTML = ",";
 }
-const operadorMultiplicar = document.getElementById('operadorMultiplicar');
 
-const botaoNumero4 = document.getElementById('botaoNumero4');
-
-function valor4() {
-    const display = document.getElementById('display')
-    display.setAttribute("value", "4")
+function calculo() {
+    var resultado = document.getElementById('resultado').innerHTML;
+    if (resultado) {
+        document.getElementById('resultado').innerHTML = eval(resultado);
+    }
 }
-const botaoNumero5 = document.getElementById('botaoNumero5');
 
-function valor5() {
-    const display = document.getElementById('display')
-    display.setAttribute("value", "5")
+function trocaValor() {
+    calculo()
+    const input = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = input * -1;
 }
-const botaoNumero6 = document.getElementById('botaoNumero6');
-
-function valor6() {
-    const display = document.getElementById('display')
-    display.setAttribute("value", "6")
-}
-const operadorSubtrair = document.getElementById('operadorSubtrair');
-
-const botaoNumero1 = document.getElementById('botaoNumero1');
-
-function valor1() {
-    const display = document.getElementById('display')
-    display.setAttribute("value", "1")
-}
-const botaoNumero2 = document.getElementById('botaoNumero2');
-
-function valor2() {
-    const display = document.getElementById('display')
-    display.setAttribute("value", "2")
-}
-const botaoNumero3 = document.getElementById('botaoNumero3');
-
-function valor3() {
-    const display = document.getElementById('display')
-    display.setAttribute("value", "3")
-}
-const operadorSoma = document.getElementById('operadorSoma');
-
-const botaoNumero0 = document.getElementById('botaoNumero0');
-
-function valor0() {
-    const display = document.getElementById('display')
-    display.setAttribute("value", "0")
-}
-const botaoVirgula = document.getElementById('botaoVirgula');
-
-function virgula() {
-    const display = document.getElementById('display')
-    display.setAttribute("value", ",")
-}
-const operadorIgualdade = document.getElementById('operadorIgualdade');
-
